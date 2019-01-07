05001221f960deb985a0ddd9b154bae17ce8cbc7
Satoshi Ninja <satoshi.ninja@protonmail.com>
2019-01-07 16:51:12 +0100

    Update veilcoin.py

--- p2pool/bitcoin/networks/veilcoin.py
+++ p2pool/bitcoin/networks/veilcoin.py
@@ -9,13 +9,13 @@ from p2pool.util import pack
 
 P2P_PREFIX='b6cfd0a3'.decode('hex')
 P2P_PORT=58810
-ADDRESS_VERSION=71
+ADDRESS_VERSION=70
 SEGWIT_ADDRESS_VERSION=5
 RPC_PORT=58812
 RPC_CHECK=lambda bitcoind: True
 SUBSIDY_FUNC=lambda height: 50*100000000 >> (height + 1)//840000
 POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('lyra2re3_hash').getPoWHash(data))
-BLOCK_PERIOD=150 # s
+BLOCK_PERIOD=60 # s
 SYMBOL='VEIL'
 CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Vertcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Vertcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.vertcoin'), 'vertcoin.conf')
 BLOCK_EXPLORER_URL_PREFIX='http://explorer.vtconline.org/block/'
