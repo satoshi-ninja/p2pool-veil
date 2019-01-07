0f8dca9f9ce32e4a0c07f8ff448e1ef590244b47
Satoshi Ninja <satoshi.ninja@protonmail.com>
2019-01-07 16:30:19 +0100

    Update veil.py

--- p2pool/bitcoin/networks/veil.py
+++ p2pool/bitcoin/networks/veil.py
@@ -8,15 +8,15 @@ from p2pool.util import pack
 
 
 P2P_PREFIX='b6cfd0a3'.decode('hex')
-P2P_PORT=5889
+P2P_PORT=58810
 ADDRESS_VERSION=71
 SEGWIT_ADDRESS_VERSION=5
-RPC_PORT=5888
+RPC_PORT=58812
 RPC_CHECK=lambda bitcoind: True
 SUBSIDY_FUNC=lambda height: 50*100000000 >> (height + 1)//840000
 POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('lyra2re3_hash').getPoWHash(data))
 BLOCK_PERIOD=150 # s
-SYMBOL='VTC'
+SYMBOL='VEIL'
 CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Vertcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Vertcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.vertcoin'), 'vertcoin.conf')
 BLOCK_EXPLORER_URL_PREFIX='http://explorer.vtconline.org/block/'
 ADDRESS_EXPLORER_URL_PREFIX='http://explorer.vtconline.org/address/'
@@ -24,4 +24,4 @@ TX_EXPLORER_URL_PREFIX='http://explorer.vtconline.org/tx/'
 SANE_TARGET_RANGE=(2**256//1000000000000000000 - 1, 2**256//100000 - 1)
 DUMB_SCRYPT_DIFF=16
 DUST_THRESHOLD=0.03e8
-HUMAN_READABLE_PART = 'vtc'
+HUMAN_READABLE_PART = 'veil'
