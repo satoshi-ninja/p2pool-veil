1abfcf2a0772282b6cb4cd0c96787e66ad9a0a16
Satoshi Ninja <satoshi.ninja@protonmail.com>
2019-01-07 16:34:04 +0100

    Update veil_tetsnet.py

--- p2pool/networks/veil_tetsnet.py
+++ p2pool/networks/veil_tetsnet.py
@@ -1,6 +1,6 @@
 from p2pool.bitcoin import networks
 
-PARENT=networks.nets['vertcoin_testnet']
+PARENT=networks.nets['veil_testnet']
 SHARE_PERIOD=15 # seconds
 CHAIN_LENGTH=24*60*60//10 # shares
 REAL_CHAIN_LENGTH=24*60*60//10 # shares
@@ -8,13 +8,13 @@ TARGET_LOOKBEHIND=200 # shares
 SPREAD=12 # blocks
 IDENTIFIER='a06a81c827cab972'.decode('hex')
 PREFIX='7c3614a6bcdcf793'.decode('hex')
-P2P_PORT=9345
+P2P_PORT=58814
 MIN_TARGET=4
 MAX_TARGET=2**256//2**20 - 1
 PERSIST=False
-WORKER_PORT=9175
-BOOTSTRAP_ADDRS='fr1.vtconline.org'.split(' ')
-ANNOUNCE_CHANNEL='#p2pool-vtc-testnet'
+WORKER_PORT=58815
+BOOTSTRAP_ADDRS='veil.pool-address.com veil.pool-address2.com'.split(' ')
+ANNOUNCE_CHANNEL='#p2pool-veil-testnet'
 VERSION_CHECK=lambda v: True
 SOFTFORKS_REQUIRED = set(['nversionbips', 'csv', 'segwit'])
 SEGWIT_ACTIVATION_VERSION = 16
