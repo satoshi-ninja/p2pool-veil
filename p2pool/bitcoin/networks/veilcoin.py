fc2421e51ffa694816745e196031b39cffdc7097
Satoshi Ninja <satoshi.ninja@protonmail.com>
2019-01-10 00:31:46 +0100

    added correct (testnet)explorer URLs

--- p2pool/bitcoin/networks/veilcoin.py
+++ p2pool/bitcoin/networks/veilcoin.py
@@ -14,13 +14,13 @@ SEGWIT_ADDRESS_VERSION=5
 RPC_PORT=58812
 RPC_CHECK=lambda bitcoind: True
 SUBSIDY_FUNC=lambda height: 50*100000000 >> (height + 1)//840000
-POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('x16r_hash').getPoWHash(data))
+POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('x16rt_hash').getPoWHash(data))
 BLOCK_PERIOD=60 # s
 SYMBOL='VEIL'
 CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'veil') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/veil/') if platform.system() == 'Darwin' else os.path.expanduser('~/.veil'), 'veil.conf')
-BLOCK_EXPLORER_URL_PREFIX='http://explorer.vtconline.org/block/'
-ADDRESS_EXPLORER_URL_PREFIX='http://explorer.vtconline.org/address/'
-TX_EXPLORER_URL_PREFIX='http://explorer.vtconline.org/tx/'
+BLOCK_EXPLORER_URL_PREFIX='https://explorer.veil-project.com/block/'
+ADDRESS_EXPLORER_URL_PREFIX='https://explorer.veil-project.com/address/'
+TX_EXPLORER_URL_PREFIX='https://explorer.veil-project.com/tx/'
 SANE_TARGET_RANGE=(2**256//1000000000000000000 - 1, 2**256//100000 - 1)
 DUMB_SCRYPT_DIFF=16
 DUST_THRESHOLD=0.03e8
--- p2pool/bitcoin/networks/veilcoin_testnet.py
+++ p2pool/bitcoin/networks/veilcoin_testnet.py
@@ -14,13 +14,13 @@ SEGWIT_ADDRESS_VERSION=196
 RPC_PORT=58813
 RPC_CHECK=lambda bitcoind: True
 SUBSIDY_FUNC=lambda height: 50*100000000 >> (height + 1)//840000
-POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('x16r_hash').getPoWHash(data))
+POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('x16rt_hash').getPoWHash(data))
 BLOCK_PERIOD=60 # s
 SYMBOL='VEILTEST'
 CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'veil') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/veil/') if platform.system() == 'Darwin' else os.path.expanduser('~/.veil'), 'veil.conf')
-BLOCK_EXPLORER_URL_PREFIX='http://test.vtc.sovereignshare.com/exp/#/vtctest/'
-ADDRESS_EXPLORER_URL_PREFIX='http://test.vtc.sovereignshare.com/exp/#/vtctest/address/'
-TX_EXPLORER_URL_PREFIX='http://test.vtc.sovereignshare.com/exp/#/vtctest/tx/'
+BLOCK_EXPLORER_URL_PREFIX='https://testnet.veil-project.com/block/'
+ADDRESS_EXPLORER_URL_PREFIX='https://testnet.veil-project.com/address/'
+TX_EXPLORER_URL_PREFIX='https://testnet.veil-project.com/tx/'
 SANE_TARGET_RANGE=(2**256//1000000000000000000 - 1, 2**256//100000 - 1)
 DUMB_SCRYPT_DIFF=256
 DUST_THRESHOLD=0.03e8
