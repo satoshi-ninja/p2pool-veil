a86d8e3b9e58b8f0a85e43a9ffa77681585d2f43
Satoshi Ninja <satoshi.ninja@protonmail.com>
2019-01-10 00:46:12 +0100

    changes to setup.py and change from veilcoin to veil

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 from setuptools import setup
 
-setup(name='p2pool-vtc',
+setup(name='p2pool-veil',
       version='1.0.0',
-      description='p2pool for Vertcoin',
-      url='http://github.com/vertcoin-project/p2pool-vtc',
+      description='p2pool for Veil',
+      url='http://github.com/satoshi-ninja/p2pool-veil',
       license='GPLv3',
-      author = 'James Lovejoy',
-      author_email = 'jameslovejoy1@gmail.com',
+      author = 'Satoshi Ninja',
+      author_email = 'satoshi.ninja@protonmail.com',
       packages=['p2pool','p2pool/bitcoin','p2pool/bitcoin/networks','p2pool/util','p2pool/networks', 
                 'nattraverso', 'nattraverso/pynupnp'],
       install_requires=[
           'twisted',
           'argparse',
-          'pyOpenSSL',
-          'lyra2re2-hash'
+          'pyOpenSSL'
       ],
       scripts=['run_p2pool.py'],
       include_package_data=True,
